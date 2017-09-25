a7dc1886480cb197feafde52c3f8d7e810357f16
cunhasb CUNHA <cunhasb@aol.com>
2017-09-25 19:41:59 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -28,7 +28,7 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
-  my_songs.collect{|song| song.key}
+  my_songs.collect{|song| puts song}
   binding.pry
 end
 
